d897e8e31409f0bbe69ba9bdc4a26d09e439ada8
Administrator <rakeshray373@gmail.com>
2024-03-03 22:07:23 +0530

    first commit

---
+++ src/main/java/com/example/demo12345/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.demo12345;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Demo12345Application.class);
+    }
+
+}
---
+++ src/test/java/com/example/demo12345/Demo12345ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo12345;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo12345ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
